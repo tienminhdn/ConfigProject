065c96a2ede58443ee7b0eed57e430383022cca3
tienminhdn <tienminhdn97@gmail.com>
2021-10-03 22:16:11 +0700

    Init project

---
+++ ConfigProject/ConfigProject/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ConfigProject
+//
+//  Created by T.Minh on 10/3/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ ConfigProject/ConfigProject/Source/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ConfigProject
+//
+//  Created by T.Minh on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
