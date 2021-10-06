1847bfddcd911fa4314e80eec15a663aacf4ae76
tienminhdn <tienminhdn97@gmail.com>
2021-10-07 01:11:47 +0700

    config delegate view

--- ConfigProject/Source/Controller/Home/HomeViewControler.swift
+++ ConfigProject/Source/Controller/Home/HomeViewControler.swift
@@ -3,15 +3,22 @@ import UIKit
 final class HomeViewControler: UIViewController {
 
     // MARK: - IBOutlets
+    @IBOutlet private weak var homeView: HomeView!
 
-    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
-        print(AppConfiguration.infoForKey(.baseURL))
-        
+        navigationController?.isNavigationBarHidden = true
+        print(AppConfiguration.infoForKey(.baseURL) ?? "minh")
+        homeView.delegate = self
     }
 
     // MARK: - Private functions
+}
+
+extension HomeViewControler: HomeViewDelegate {
+    func vc(_ vc: HomeView, needsPerform action: HomeView.Action) {
+        print("tab button")
+    }
 }
--- ConfigProject/Source/Controller/View/HomeView.swift
+++ ConfigProject/Source/Controller/View/HomeView.swift
@@ -1,17 +1,34 @@
 import UIKit
 
+protocol HomeViewDelegate: AnyObject {
+    func vc(_ vc: HomeView, needsPerform action: HomeView.Action)
+}
+
 final class HomeView: BaseView {
 
     // MARK: - IBOutlets
-
+    @IBOutlet private weak var tapButton: UIButton!
+    
+    weak var delegate: HomeViewDelegate?
     var viewModel: HomeViewVM = HomeViewVM() {
         didSet {
             updateView()
         }
     }
 
+    // MARK: - IBActions
+    @IBAction func tapButtonTouchUpInside(_ sender: UIButton) {
+        delegate?.vc(self, needsPerform: .tabButton)
+    }
+    
     // MARK: - Private functions
     private func updateView() {
         //
+    }
+}
+
+extension HomeView {
+    enum Action {
+        case tabButton
     }
 }
